f4fcd23025b63bf8f415d412472035af59493649
BEN TAALA Wassim <wbentaala.ext@sdlnet.fr>
2018-10-05 18:12:58 +0200

    First commit

---
+++ src/app/model/recipe.ts
@@ -0,0 +1,29 @@
+export class Ingredient{
+    ingredient: string;
+    measure: string;
+}
+
+export class Instruction{
+    instruction: string;
+    photo: string;
+}
+
+
+export class Recipe{
+    
+    public title: string;
+    public description: string; 
+    public ingredients: Ingredient[];
+    public instructions: Instruction[];
+    public cover_photo: string;
+
+    constructor(t: string, d: string, ingr: Ingredient[], instr: Instruction[], cp: string ){
+        this.title = t;
+        this.description = d;
+        this.ingredients = ingr;
+        this.instructions = instr;
+        this.cover_photo = cp;
+    }
+
+    
+}
