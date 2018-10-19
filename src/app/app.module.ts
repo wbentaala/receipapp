44d4a8728f994668cf2f142a5168a48bf015dae7
Wassim <wbentaala.ext@louvre-hotels.com>
2018-10-19 18:12:17 +0200

    refaire les styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
@@ -14,7 +15,18 @@ import { RecipeSummaryComponent } from './components/recipe-summary/recipe-summa
   ],
   imports: [
     BrowserModule, 
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'recipes',
+        component : RecipeListComponent
+      },
+      {
+        path: '',
+        redirectTo: '/recipes', 
+        pathMatch: 'full'
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
