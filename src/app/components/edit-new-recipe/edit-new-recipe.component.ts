3ba5fa1343260b53cc04143756d98892e66b3421
wassim <wassim.bentaala@gmail.com>
2018-10-28 19:47:34 +0100

    add newrecipe ingredients and instructions

--- src/app/components/edit-new-recipe/edit-new-recipe.component.ts
+++ src/app/components/edit-new-recipe/edit-new-recipe.component.ts
@@ -9,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class EditNewRecipeComponent implements OnInit {
 
   recipe_in_progress: Recipe;
-  constructor() { }
+  constructor() { 
+    this.recipe_in_progress = Recipe.createEmptyRecipe();
+  }
 
   ngOnInit() {
+  }
+
+  public addIngredientPressed(){
+    if(!this.recipe_in_progress.ingredients){
+      this.recipe_in_progress.ingredients = [{ ingredient : null, measure: null}];
+    }
+    else{
+      this.recipe_in_progress.ingredients.push({ingredient: null, measure: null});
+    }
+  }
+
+  public removeIngredient(index){
+    this.recipe_in_progress.ingredients.splice(index, 1);
+  }
+
+  public addInstructionPressed(){
+    if(!this.recipe_in_progress.instructions){
+      this.recipe_in_progress.instructions = [ { instruction : null, photo : null }];
+    }
+    else{
+      this.recipe_in_progress.instructions.push({ instruction : null, photo : null });
+    }
+  }
+
+  public removeInstruction(index){
+    this.recipe_in_progress.instructions.splice(index, 1);
   }
 
 }
